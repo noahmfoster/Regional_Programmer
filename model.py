47315bd219b18e849be4f315ad54f2fa005c69d7
usernamenoahfoster <48572872+usernamenoahfoster@users.noreply.github.com>
2022-12-09 10:06:24 -0500

    more work

--- model.py
+++ model.py
@@ -7,8 +7,6 @@ class ScenePrompt():
     def __init__(self, characters = [], nouns = [], first_line = "", first_character = ""): # lines is a list of tuples of (speaker, line)
         self.characters = characters if type(characters) == list else [characters]
 
-        self.first_line = f"{first_character}: {first_line}"
-
         self.nouns = nouns 
 
         if first_character:
@@ -16,46 +14,56 @@ class ScenePrompt():
         elif self.characters: self.first = tf.random.shuffle(self.characters)[0]
         else: self.first = "Dwight:"
 
+        self.first_line = f"{self.first}:" + (f" {first_line}" if first_line else "")
+
     def to_text(self):
         output = f"Characters: " + ", ".join(set(self.characters)) + "\n\n"
 
         if self.nouns:
-            output += "Nouns: " + ", ".join(self.nouns) + "\n\n" # Need to implement random sampling later
+            output += "Nouns: " + ", ".join(self.nouns) + "\n\n"
 
         output += "----TEXT----"
 
-        output += "\n\n".join(
-            [
-                f"{character}: {line}"
-                for character, line in zip(
-                    self.characters[:self.n_lines],
-                    self.lines[:self.n_lines]
-                )
-            ]
-        )
+        output += "\n\n" + self.first_line
 
         return output
 
 class AtRM():
-    def __init__(self, verbose = False):
+    def __init__(self, verbose = False, lm = "tuned"):
         if verbose: print("Loading Vision models...")
-        self.clip_model, self.processor, self.face_model, self.facecascade = get_models()
+        try:
+            self.clip_model, self.processor, self.face_model, self.facecascade = get_models()
+        except (FileNotFoundError, OSError):
+            print("Something is missing!")
+
         self.nouns, self.words = get_nouns()
         if verbose:
             print("Vision models loaded.")
             print("Loading Language models...")
 
-        self.lm, self.tokenizer = get_language_model()
+        self.lm, self.tokenizer = get_language_model(model_name = lm)
+
         if verbose: print("Language models loaded.")
 
         self.device = "cuda" if is_available() else "cpu"
         self.lm.to(self.device)
         self.tokenizer.to(self.device)
+    
+    def init_overide(self, clip_model = None, processor = None, face_model = None, facecascade = None, nouns = None, words = None, lm = None, tokenizer = None, device = None):
+        if clip_model: self.clip_model = clip_model
+        if processor: self.processor = processor
+        if face_model: self.face_model = face_model
+        if facecascade: self.facecascade = facecascade
+        if nouns: self.nouns = nouns
+        if words: self.words = words
+        if lm: self.lm = lm
+        if tokenizer: self.tokenizer = tokenizer
+        if device: self.device = device
 
     def __str__(self) -> str:
-        return f"Your Assistant (to) the Regional Manager is here"
+        return f"Your Assistant (to) the Regional Manager is here!"
 
-    def __call__(self, img_file, first_line = "", first_character = "", include_nouns = True, include_prompt = True):
+    def __call__(self, img_file, first_character = "", first_line = "", include_nouns = True, include_prompt = True):
         character_vector, nouns = run_vision_model(
             img_file,
             self.clip_model,
