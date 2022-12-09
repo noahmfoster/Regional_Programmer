3ccdd8698091a5d33fe66cee68f946aa6c0557b7
Chloe Griffin <106169337+dcgriffi@users.noreply.github.com>
2022-12-08 23:26:06 -0500

    Delete pipeline.py

--- VisionModel/pipeline.py
+++
@@ -1,3 +0,0 @@
-#This file will be used to build a pipeline between the image and language model.
-#Ideally it will take the vector of words outputted from the image model and
-#rewrite this as a prompt for the language model. 
